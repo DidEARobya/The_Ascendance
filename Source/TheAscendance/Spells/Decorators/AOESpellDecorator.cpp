25a613b38a124b3c29586dea1fa69b87c707b8a3
Ben Higham <94679239+DidEARobya@users.noreply.github.com>
2025-09-27 02:58:55 +0100

    Core Effect Data Classes Setup

--- Source/TheAscendance/Spells/Decorators/AOESpellDecorator.cpp
+++ Source/TheAscendance/Spells/Decorators/AOESpellDecorator.cpp
@@ -101,26 +101,3 @@ void UAOESpellDecorator::ProcessHitDamage(int& damage, FVector targetLocation, F
 
 	damage += damageWithFalloff;
 }
-
-bool UAOESpellDecorator::CastSpell()
-{
-	if (m_DecoratedSpell == nullptr)
-	{
-		LOG_ERROR("AOESpellDecorator has no decorated spell");
-		return false;
-	}
-
-	FVector unitDirection = m_DecoratedSpell->GetSpellOwner()->GetCastStartForward();
-	unitDirection.Normalize();
-
-	Fire(unitDirection);
-
-	return true;
-}
-
-void UAOESpellDecorator::Fire(FVector direction)
-{
-	m_DecoratedSpell->Fire(direction);
-
-	LOG_ONSCREEN(-1, 1.0f, FColor::Cyan, "AOE Decorator: Fire");
-}
